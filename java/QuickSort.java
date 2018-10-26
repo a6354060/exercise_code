36438e0c6b05c7ef5a79df41cd0f29edd0d9fe0e
乐于分享 <15111397674@163.com>
2018-10-26 10:07:45 +0800

    Update QuickSort.java

--- java/QuickSort.java
+++ java/QuickSort.java
@@ -8,7 +8,7 @@ public class QuickSort{
 
           while(i<j){
 		     
-		    while(i<j&&r[i]<=r[j]){
+		    while(i<j && r[i]<=r[j]){
 			   j--;
 			}
              if(i<j){
@@ -18,7 +18,7 @@ public class QuickSort{
 				i++;
 			 }
 
-          while(i<j&&r[i]<=r[j]){
+          while(i<j && r[i]<=r[j]){
 		      i++;
 		  }              
 		     if(i<j){
@@ -35,22 +35,19 @@ public class QuickSort{
 
    public static void quickSort(int []a,int first,int end){
         
-	      int p=0;
-		  if(first<end){
-		     p = partition(a,first,end);
-             quickSort(a,first,p-1);			 
-			 quickSort(a,p+1,end);
-		  
-		  }
-   }
-
-  public static void main(String args[]){
-     int a[]=new int[]{3,5,45,6,7,9,78,8};
-	 
-      quickSort(a,0,a.length-1);
-     for(int i=0;i<a.length;i++){
-	    System.out.print(a[i]+",");
-	 }
+      int p=0;
+	if(first<end){
+     		p = partition(a,first,end);
+		quickSort(a,first,p-1);			 
+		quickSort(a,p+1,end);
+  	}
   }
 
+  public static void main(String args[]){
+	int a[]=new int[]{3,5,45,6,7,9,78,8};
+	quickSort(a,0,a.length-1);
+	for(int i=0;i<a.length;i++){
+    		System.out.print(a[i]+",");
+	}
+  }
 }
